239cd83d1acbbff0ca9b412cf6296bf2b1bee911
NeonChrome93 <daarksky1919@gmail.com>
2024-04-25 23:45:09 +0300

    renamed to "entity"

--- src/features/public/devices/domain/device.entity.ts
+++ src/features/public/devices/domain/device.entity.ts
@@ -1,7 +1,20 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { User } from '../../../admin/users/domain/user.entity';
+
+@Entity()
 export class Device {
+    @Column()
     ip: string;
+    @PrimaryColumn()
     deviceId: string;
+    @Column()
     userId: string;
+    @Column()
     title: string;
+    @Column()
     lastActiveDate: Date;
+
+    @ManyToOne(() => User, user => user.devices)
+    @JoinColumn({ name: 'userId' })
+    user: User;
 }

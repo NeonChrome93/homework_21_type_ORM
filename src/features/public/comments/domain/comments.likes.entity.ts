9d4212c0e60c1e031ebff474d192a9f2fc6ee43d
NeonChrome93 <daarksky1919@gmail.com>
2024-05-16 23:53:13 +0300

    added likes entity for comments and posts

---
+++ src/features/public/comments/domain/comments.likes.entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryColumn } from 'typeorm';
+import { REACTIONS_ENUM } from '../api/models/output/comments.output.models';
+import { User } from '../../../admin/users/domain/user.entity';
+
+@Entity()
+export class Comments_likes {
+    @PrimaryColumn('uuid')
+    id: string;
+    @Column()
+    userId: string;
+    @Column()
+    createdAt: Date;
+    @Column({
+        type: 'enum',
+        enum: REACTIONS_ENUM,
+        default: REACTIONS_ENUM.None,
+    })
+    status: REACTIONS_ENUM;
+    @Column()
+    commentId: string;
+
+    @OneToOne(() => User, user => user.likes)
+    @JoinColumn({ name: 'userId' })
+    public user = User;
+
+    @ManyToOne(() => Comment, comment => comment.likes)
+    @JoinColumn({ name: 'commentId' })
+    comment: Comment;
+}
---
+++ src/features/public/posts/domain/post.lilkes.entity.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryColumn } from 'typeorm';
+import { REACTIONS_ENUM } from '../../comments/api/models/output/comments.output.models';
+import { Post } from './post.entity';
+import { User } from '../../../admin/users/domain/user.entity';
+
+@Entity()
+export class Post_likes {
+    @PrimaryColumn('uuid')
+    id: string;
+    @Column()
+    userId: string;
+    @Column()
+    createdAt: Date;
+    @Column({
+        type: 'enum',
+        enum: REACTIONS_ENUM,
+        default: REACTIONS_ENUM.None,
+    })
+    status: REACTIONS_ENUM;
+    @Column()
+    postId: string;
+
+    @OneToOne(() => User, user => user.likes)
+    @JoinColumn({ name: 'userId' })
+    public user = User;
+
+    @ManyToOne(() => Post, post => post.likes)
+    @JoinColumn({ name: 'postId' })
+    post: Post;
+}

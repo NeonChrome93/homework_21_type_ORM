42ac47d495df83eeaf022af8e0f9f05f9c67ed5b
NeonChrome93 <daarksky1919@gmail.com>
2024-06-03 23:59:42 +0300

    add tests for users

---
+++ src/test/users.e2e.spec.ts
@@ -0,0 +1,61 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../app.module';
+import { agent as request } from 'supertest';
+
+const createUser = {
+    login: 'Date',
+    email: 'y.smirnow@yandex.ru',
+    password: '123456',
+};
+
+const createUser2 = {
+    login: 'Trasher',
+    email: 'y.trhash@yandexx.ru',
+    password: '1234562',
+};
+
+const headers = {
+    Authorization: 'Basic YWRtaW46cXdlcnR5',
+};
+
+describe('Users API', () => {
+    let app: INestApplication;
+
+    beforeEach(async () => {
+        console.log('ENV in TESTS', process.env.ENV);
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+        // .overrideProvider(EmailAdapter)
+        // .useClass(EmailAdapterMock)
+        // .compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
+
+    it('Before all', async () => {
+        await request(app.getHttpServer()).delete('/testing/all-data').expect(204);
+    });
+
+    it('Get all users', async () => {
+        await request(app.getHttpServer())
+            .get('/sa/users')
+            .set(headers)
+            .expect(200, { pagesCount: 1, page: 1, pageSize: 10, totalCount: 0, items: [] });
+    });
+
+    it('Create User', async () => {
+        await request(app.getHttpServer()).post('/users').set(headers).send(createUser).expect(201);
+    });
+
+    it('Get User', async () => {
+        await request(app.getHttpServer()).get('/users').set(headers).expect(200);
+    });
+
+    it('Delete User ', async () => {
+        const user = await request(app.getHttpServer()).post('/users').set(headers).send(createUser2);
+        await request(app.getHttpServer()).delete(`/users/${user.body.id}`).set(headers).expect(204);
+    });
+});

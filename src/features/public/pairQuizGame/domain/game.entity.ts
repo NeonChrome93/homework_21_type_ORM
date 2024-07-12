2f91fc533fc6109809e7fcc67d737e5ce847c6de
NeonChrome93 <daarksky1919@gmail.com>
2024-07-12 20:12:12 +0300

    added 3 dates ror game entity

--- src/features/public/pairQuizGame/domain/game.entity.ts
+++ src/features/public/pairQuizGame/domain/game.entity.ts
@@ -7,11 +7,21 @@ export class GameEntity {
     id: string;
 
     @Column({ nullable: true })
-    firstPlayer: string;
+    firstPlayerProgress: string;
 
     @Column({ nullable: true })
-    secondPlayer: string;
+    secondPlayerProgress: string;
 
     @Column({ type: 'enum', enum: GAME_STATUS, default: GAME_STATUS.Pending })
     status: GAME_STATUS;
+
+    // todo add 3 dates
+    @Column()
+    pairCreatedDate: Date;
+
+    @Column({ nullable: true })
+    startGameDate: Date;
+
+    @Column({ nullable: true })
+    finishGameDate: Date;
 }

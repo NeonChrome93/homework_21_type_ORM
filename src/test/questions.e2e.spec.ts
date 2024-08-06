0352575d27b92383d8083e162f10af29fde64ea3
NeonChrome93 <daarksky1919@gmail.com>
2024-08-06 23:59:29 +0300

    tests for question logic

---
+++ src/test/questions.e2e.spec.ts
@@ -0,0 +1,56 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../app.module';
+import { agent as request } from 'supertest';
+
+const headers = {
+    Authorization: 'Basic YWRtaW46cXdlcnR5',
+};
+
+const createQuestion = {
+    body: 'ivan',
+    correctAnswers: ['str', 'fdsdsfdsf'],
+};
+
+const updateQuestion = {
+    body: 'ivar',
+    correctAnswers: ['str', 'nostr'],
+};
+
+describe('Question API', () => {
+    let app: INestApplication;
+
+    beforeEach(async () => {
+        console.log('ENV in TESTS', process.env.ENV);
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
+
+    it('Before all', async () => {
+        await request(app.getHttpServer()).delete('/testing/all-data').expect(204);
+    });
+
+    it('Get All Questions ', async () => {
+        await request(app.getHttpServer())
+            .get('/sa/quiz/questions')
+            .set(headers)
+            .expect(200, { pagesCount: 0, page: 1, pageSize: 10, totalCount: 0, items: [] });
+    });
+
+    it('Create Question', async () => {
+        await request(app.getHttpServer()).post('/sa/quiz/questions').set(headers).send(createQuestion).expect(201);
+    });
+
+    it('Update Question', async () => {
+        const question = await request(app.getHttpServer())
+            .post('/sa/quiz/questions')
+            .set(headers)
+            .send(createQuestion)
+            .expect(201);
+        await request(app.getHttpServer()).put('/sa/quiz/questions').set(headers).send(updateQuestion).expect(204);
+    });
+});
